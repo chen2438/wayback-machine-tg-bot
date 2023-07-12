fbdfbb11fd17b7214bb99f68c179dcbe6d5ecaa3
chen2438 <2363822647@qq.com>
2023-07-12 20:30:22 +0800

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,67 @@
 # wayback-machine-tg-bot
+
+## 运行环境
+
+- node.js
+- npm
+
+## 部署
+
+下载项目
+
+```bash
+git clone https://github.com/chen2438/wayback-machine-tg-bot.git
+cd wayback-machine-tg-bot
+npm install
+```
+
+在 https://t.me/BotFather 创建你的 Telegram Bot 并获取 token
+
+![image-20230712201123310](https://media.opennet.top/i/2023/07/12/64ae986f283a9.png)
+
+加载你的 Telegram Bot HTTP API Token
+
+```bash
+export tg_token=你的 API Token
+```
+
+启动应用
+
+```bash
+node wayback-machine-tg-bot.js
+```
+
+使用 pm2 启动应用，持久化应用程序
+
+```bash
+# Ctrl+C 关闭刚才启动的node.js程序
+npm install -g pm2
+pm2 start wayback-machine-tg-bot.js
+pm2 save
+pm2 startup
+```
+
+## 运行演示
+
+```
+Me: 
+/start
+
+Bot: 
+欢迎使用 wayback-machine-tg-bot !
+使用方法:
+1. 发送一个网址给我。
+2. 我会尝试将这个网址保存到Wayback Machine。
+3. 如果成功，我会发送一个存档后的网址给你。
+
+Me:
+https://icp.gov.moe/aboutus.php
+
+Bot:
+成功触发了Wayback Machine的保存过程。
+存档版本位于: http://web.archive.org/web/20230712121858/https://icp.gov.moe/aboutus.php
+```
+
+
+
+![image-20230712201956247](https://media.opennet.top/i/2023/07/12/64ae9a6fa2010.png)
---
+++ wayback-machine-tg-bot.js
@@ -0,0 +1,50 @@
+const TelegramBot = require('node-telegram-bot-api');
+const http = require('http');
+const url = require('url');
+
+// 替换为你从@BotFather获得的Telegram bot的token
+const tg_token = process.env.tg_token;
+
+// 创建一个使用'polling'获取新更新的bot
+const bot = new TelegramBot(tg_token, {polling: true});
+
+// 监听/start命令
+bot.onText(/\/start/, (msg) => {
+    const chatId = msg.chat.id;
+    bot.sendMessage(chatId, `欢迎使用 wayback-machine-tg-bot !\n使用方法:\n1. 发送一个网址给我。\n2. 我会尝试将这个网址保存到Wayback Machine。\n3. 如果成功，我会发送一个存档后的网址给你。`);
+});
+
+bot.on('message', (msg) => {
+    const chatId = msg.chat.id;
+    let urlToSave = msg.text;
+
+    // 如果接收到的是/start命令，直接返回
+    if (urlToSave === '/start') {
+        return;
+    }
+
+    // 验证接收到的文本是否是一个有效的URL
+    try {
+        new URL(urlToSave);
+    } catch (err) {
+        bot.sendMessage(chatId, `错误: 无法识别的URL。`);
+        return;
+    }
+    bot.sendMessage(chatId, `已接收到URL, 正在处理中。`);
+    let waybackUrl = `http://web.archive.org/save/${urlToSave}`;
+
+    http.get(waybackUrl, (res) => {
+        let statusCode = res.statusCode;
+
+        if (statusCode === 302) {
+            // 如果成功触发了Wayback Machine的保存过程，发送一条消息并附上存档版本的URL
+            bot.sendMessage(chatId, `成功触发了Wayback Machine的保存过程。\n存档版本位于: ${res.headers.location}`);
+        } else {
+            // 如果保存失败，发送一条消息
+            bot.sendMessage(chatId, `保存${urlToSave}到Wayback Machine失败。状态码: ${statusCode}`);
+        }
+    }).on('error', (e) => {
+        // 如果请求过程中发生错误，发送一条消息
+        bot.sendMessage(chatId, `发生错误：${e.message}`);
+    });
+});
